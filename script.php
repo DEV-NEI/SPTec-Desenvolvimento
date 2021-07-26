1d34997b2911152bed3db40ca7cc8cc6aa801ff0
Dev-Nei <dev-nei@hotmail.com>
2021-07-26 17:58:22 -0300

    var_dump

--- script.php
+++ script.php
@@ -16,6 +16,9 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+//var_dump($nome);
+//var_dump($idade);
+//return 0;
  if($idade >= 6 && $idade <=12 )
  {
  	for($i = 0; $i <= count($categorias); $i++)
